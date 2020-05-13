f7e60963a9dac8725090907bd0184d9b650ba272
Valtteri <n6niva00@students.oamk.fi>
2020-05-13 20:36:47 +0300

    3.2/6

---
+++ app/Controllers/Todo.php
@@ -0,0 +1,36 @@
+<?php namespace App\Controllers;
+use App\Models\TodoModel;
+
+class Todo extends BaseController
+{
+    public function index()
+    {
+        $model = new TodoModel();
+        $data['title'] = 'Todo';
+        $data['todos'] = $model->getTodos();
+        
+        echo view('templates/header', $data);
+        echo view('todo/list', $data);
+        echo view('templates/footer', $data);
+    }
+    
+    public function create(){
+        $model = new TodoModel();
+        
+        if(!$this->validate([
+            'title' => 'required|max_length[255]',
+        ])){
+            echo view('templates/header', ['title' => 'Add new task']);
+            echo view('todo/create');
+            echo view('templates/footer');
+            
+        }
+        else{
+            $model->save([
+               'title' => $this->request->getVar('title'),
+                'description' => $this->request->getVar('description')
+            ]);
+            return redirect('todo');
+        }
+    }
+}
